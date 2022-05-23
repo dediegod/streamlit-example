c11bea0c63a25ebcc9a46ff7b79a58e85bdd22d2
dediegod <106017815+dediegod@users.noreply.github.com>
2022-05-23 19:38:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,27 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+import plotly.express as px  # interactive charts
 
+https://github.com/dediegod/streamlit-example/blob/master/penguins%20lter.csv
 """
-# Welcome to Streamlit!
+url = 'https://github.com/dediegod/streamlit-example/blob/master/penguins%20lter.csv'
+df = pd.read_csv(url)
+#print(df.head(5))
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+primaryColor="#6eb1ff"
+backgroundColor="#f1eded"
+secondaryBackgroundColor="#e4e4e4"
+textColor="#000b4c"
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#print('Cantidad de Filas y columnas:',df.shape)
+#print('Nombre columnas:',df.columns)
+#df.info()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+df["Sex"] = df["Sex"].fillna("Sexo sin identificar")
+df["Comments"] = df["Comments"].fillna("")
+df["Sex"] = df["Sex"].replace(to_replace = ["."], value ="Sexo sin identificar")
+df["Sex"] = df["Sex"].replace(to_replace = ["MALE"], value ="Male")
+df["Sex"] = df["Sex"].replace(to_replace = ["FEMALE"], value ="Female")
+df["Sex"] = df["Sex"].dropna()
+df = df.dropna()
+df = df.drop(['Sample Number', 'Region'], axis=1)
